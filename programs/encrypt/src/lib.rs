1949a7d620895f363add32edc40b5553ececeb4b
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2022-06-05 16:36:01 +0530

    Update lib.rs

--- programs/encrypt/src/lib.rs
+++ programs/encrypt/src/lib.rs
@@ -1,9 +1,8 @@
 use anchor_lang::prelude::*;
 //use spl_token_2022::solana_zk_token_sdk::encryption::{elgamal, pedersen};
-use spl_token_2022::solana_zk_token_sdk::zk_token_elgamal::{ops::{add, subtract},pod::{ ElGamalPubkey, ElGamalCiphertext}};
+use spl_token_2022::solana_zk_token_sdk::zk_token_elgamal::{ops::{add, subtract, add_to, subtract_from},pod::{ ElGamalPubkey, ElGamalCiphertext}};
 use std::io::{self, Write};
 use std::ops::Deref;
-use bytemuck::{Zeroable, Pod};
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
@@ -24,16 +23,32 @@ pub mod encrypt {
         let ct_1 = &amount2_ct;
         let sum = add(ct_0, ct_1).unwrap();
         let calc_state = &mut ctx.accounts.calc.load_mut()?;
-        calc_state.user_result_ct = ElCipher::from(sum);
-        msg!("Resulting sum of ciphertexts: {:?}", calc_state.user_result_ct);
+        calc_state.sum_of_cts = ElCipher::from(sum);
+        msg!("Resulting sum of ciphertexts: {:?}", calc_state.sum_of_cts);
         Ok(())   
     }
     pub fn subtract_cts(ctx: Context<Subtract>, amount1_ct: ElCipher, amount2_ct: ElCipher) -> Result<()> {
        //homomorphic subtraction of two ciphertexts
         let difference = subtract(&amount1_ct, &amount2_ct).unwrap();
         let calc_state = &mut ctx.accounts.calc.load_mut()?;
-        calc_state.user_result_ct = ElCipher::from(difference);
-        msg!("Resulting difference of ciphertexts: {:?}", calc_state.user_result_ct);
+        calc_state.diff_of_cts = ElCipher::from(difference);
+        msg!("Resulting difference of ciphertexts: {:?}", calc_state.diff_of_cts);
+        Ok(())   
+    }
+    pub fn add_to_cts(ctx: Context<AddTo>, amount_ct: ElCipher, amount_normal: u64) -> Result<()> {
+        //adds a normal u64 number to an existing cyphertext.
+        let calc_state = &mut ctx.accounts.calc.load_mut()?;
+        let new_sum = add_to(&amount_ct, amount_normal).unwrap();
+        calc_state.sum_of_cts = ElCipher::from(new_sum);
+        msg!("ciphertext after addition: {:?}", calc_state.added_cts);
+        Ok(())   
+    }
+    pub fn subtract_from_cts(ctx: Context<SubtractFrom>, amount_ct: ElCipher, amount_normal: u64) -> Result<()> {
+        //subtracts a normal u64 number from an existing cyphertext.
+        let calc_state = &mut ctx.accounts.calc.load_mut()?;
+        let new_diff = subtract_from(&amount_ct, amount_normal).unwrap();
+        calc_state.diff_of_cts = ElCipher::from(new_diff);
+        msg!("ciphertext after subtraction: {:?}", calc_state.diff_of_cts);
         Ok(())   
     }
 }
@@ -56,12 +71,24 @@ pub struct Subtract<'info>{
     #[account(mut)]
     calc: AccountLoader<'info, ZkCalc>,
 }
+#[derive(Accounts)]
+pub struct AddTo<'info>{
+    #[account(mut)]
+    calc: AccountLoader<'info, ZkCalc>,
+}
+#[derive(Accounts)]
+pub struct SubtractFrom<'info>{
+    #[account(mut)]
+    calc: AccountLoader<'info, ZkCalc>,
+}
 
 #[account(zero_copy)]
 #[repr(packed)]
 pub struct ZkCalc{
     user_elgamal_key: ElGamalKey,
-    user_result_ct: ElCipher,
+    sum_of_cts: ElCipher,
+    diff_of_cts: ElCipher,
+    added_cts: ElCipher,
     bump: u8,
 } 
 
@@ -91,7 +118,8 @@ impl From<ElGamalCiphertext> for ElCipher{
     }
 }  
 
-
+//unsafe impl Zeroable for ElGamalKey {}
+//unsafe impl Pod for ElGamalKey {}
 impl AnchorSerialize for ElGamalKey {
     fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
         let buf = bytemuck::bytes_of(&self.0);
@@ -111,7 +139,7 @@ impl AnchorDeserialize for ElGamalKey {
 impl AnchorSerialize for ElCipher {
     fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
         let buf = bytemuck::bytes_of(&self.0);
-        writer.write_all(buf)?;
+         writer.write_all(buf)?;
         Ok(())
     }
 }
